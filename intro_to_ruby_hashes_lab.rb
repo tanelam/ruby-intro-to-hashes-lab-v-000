1069ca62d6586fb8cb5b72fa516a3110e6047042
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-17 20:14:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,11 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => {
-		:pieces => 4,
-		:names => {},
-		:rent_in_dollars => {}
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {},
 		}
 	}
 
